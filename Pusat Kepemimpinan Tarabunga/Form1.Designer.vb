9b573a2f04561c7fba8a8162247231ba2fc07fb5
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-06-03 00:04:25 +0700

    Add project files.

---
+++ Pusat Kepemimpinan Tarabunga/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.listDataPenjualan = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileKeluar = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLaporan = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLaporanMasukkan = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLaporanTampilkan = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelpTentang = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.labelPendapatanTotal = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.listDataPenjualan)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 59)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(399, 266)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Penjualan"
+        '
+        'listDataPenjualan
+        '
+        Me.listDataPenjualan.FormattingEnabled = True
+        Me.listDataPenjualan.Location = New System.Drawing.Point(7, 20)
+        Me.listDataPenjualan.Name = "listDataPenjualan"
+        Me.listDataPenjualan.Size = New System.Drawing.Size(386, 238)
+        Me.listDataPenjualan.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuLaporan, Me.mnuHelp})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(424, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileKeluar})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuFileKeluar
+        '
+        Me.mnuFileKeluar.Name = "mnuFileKeluar"
+        Me.mnuFileKeluar.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.K), System.Windows.Forms.Keys)
+        Me.mnuFileKeluar.Size = New System.Drawing.Size(148, 22)
+        Me.mnuFileKeluar.Text = "&Keluar"
+        '
+        'mnuLaporan
+        '
+        Me.mnuLaporan.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuLaporanMasukkan, Me.mnuLaporanTampilkan})
+        Me.mnuLaporan.Name = "mnuLaporan"
+        Me.mnuLaporan.Size = New System.Drawing.Size(62, 20)
+        Me.mnuLaporan.Text = "&Laporan"
+        '
+        'mnuLaporanMasukkan
+        '
+        Me.mnuLaporanMasukkan.Name = "mnuLaporanMasukkan"
+        Me.mnuLaporanMasukkan.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.M), System.Windows.Forms.Keys)
+        Me.mnuLaporanMasukkan.Size = New System.Drawing.Size(272, 22)
+        Me.mnuLaporanMasukkan.Text = "&Masukkan Data Penjualan"
+        '
+        'mnuLaporanTampilkan
+        '
+        Me.mnuLaporanTampilkan.Name = "mnuLaporanTampilkan"
+        Me.mnuLaporanTampilkan.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.T), System.Windows.Forms.Keys)
+        Me.mnuLaporanTampilkan.Size = New System.Drawing.Size(272, 22)
+        Me.mnuLaporanTampilkan.Text = "&Tampilkan Laporan Penjualan"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuHelpTentang})
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(44, 20)
+        Me.mnuHelp.Text = "&Help"
+        '
+        'mnuHelpTentang
+        '
+        Me.mnuHelpTentang.Name = "mnuHelpTentang"
+        Me.mnuHelpTentang.Size = New System.Drawing.Size(152, 22)
+        Me.mnuHelpTentang.Text = "&Tentang"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(165, 349)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Pendapatan Total"
+        '
+        'labelPendapatanTotal
+        '
+        Me.labelPendapatanTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelPendapatanTotal.Location = New System.Drawing.Point(263, 345)
+        Me.labelPendapatanTotal.Name = "labelPendapatanTotal"
+        Me.labelPendapatanTotal.Size = New System.Drawing.Size(149, 23)
+        Me.labelPendapatanTotal.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 386)
+        Me.Controls.Add(Me.labelPendapatanTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Pusat Kepemimpinan Tarabunga"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents listDataPenjualan As System.Windows.Forms.ListBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFileKeluar As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLaporan As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLaporanMasukkan As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLaporanTampilkan As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuHelp As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuHelpTentang As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents labelPendapatanTotal As System.Windows.Forms.Label
+
+End Class
---
+++ Pusat Kepemimpinan Tarabunga/Form1.vb
@@ -0,0 +1,120 @@
+﻿Public Class Form1
+    'aplikasi ini menampilkan sebuah laporan penjualan untuk
+    'pusat kepemimpinan Tarabung 
+
+    ' Class-level declarations
+    Const intSUBSKRIPT_MAKS As Integer = 4              'subskript atas
+    Dim strNamaProduk(intSUBSKRIPT_MAKS) As String      'nama produk
+    Dim strPenjelasan(intSUBSKRIPT_MAKS) As String      'penjelasan
+    Dim intNomorProd(intSUBSKRIPT_MAKS) As Integer      'nomor produk
+    Dim decHarga(intSUBSKRIPT_MAKS) As Decimal          'harga per unit
+    Dim intUnitTerjual(intSUBSKRIPT_MAKS) As Integer    'banyak unit terjual
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'menginisialisasi array dengan data produk
+        AwalArray()
+    End Sub
+
+    Private Sub AwalArray()
+        'menginisialisasi array
+        'produk pertama
+        strNamaProduk(0) = "Enam Langkah Kepemimpinan"
+        strPenjelasan(0) = "Buku"
+        intNomorProd(0) = 914
+        decHarga(0) = 12950D
+
+        'produk kedua
+        strNamaProduk(1) = "Enam Langkah Kepemimpinan"
+        strPenjelasan(1) = "CD"
+        intNomorProd(1) = 915
+        decHarga(1) = 14950D
+
+        'produk ketiga
+        strNamaProduk(2) = "Jalan Kesuksesan"
+        strPenjelasan(2) = "Video"
+        intNomorProd(2) = 916
+        decHarga(2) = 18950D
+
+        'produk keempat
+        strNamaProduk(3) = "Tujuh Pelajaran Keberhasilan"
+        strPenjelasan(3) = "Buku"
+        intNomorProd(3) = 917
+        decHarga(3) = 16950D
+
+        'produk kelima
+        strNamaProduk(4) = "Tujuh Pelajaran Keberhasilan"
+        strPenjelasan(4) = "CD"
+        intNomorProd(4) = 918
+        decHarga(4) = 21950D
+    End Sub
+
+
+    Private Sub mnuFileKeluar_Click(sender As Object, e As EventArgs) Handles mnuFileKeluar.Click
+        'menutup form
+        Me.Close()
+    End Sub
+
+    Private Sub mnuLaporanMasukkan_Click(sender As Object, e As EventArgs) Handles mnuLaporanMasukkan.Click
+        Dim intHitung As Integer = 0    'pencacah loop
+
+        Do While intHitung <= intSUBSKRIPT_MAKS
+            Try
+                'membaca banyak unit terjual untuk sebuah produk
+                intUnitTerjual(intHitung) = CInt(
+                 InputBox("Masukkan banyak unit terjual dari nomor produk " &
+                 intNomorProd(intHitung)))
+
+                'menginkremen intHitung
+                intHitung += 1
+            Catch
+                'pesan error untuk masukan tak valid
+                MessageBox.Show("Masukkan integer.")
+            End Try
+        Loop
+    End Sub
+
+    Private Sub mnuLaporanTampilkan_Click(sender As Object, e As EventArgs) Handles mnuLaporanTampilkan.Click
+        'menghitung dan menampilkan pendapatan untuk tiap
+        'produk dan pendapatan total
+        Dim intHitung As Integer
+        Dim decPendapatan As Decimal
+        Dim decPendapatanTotal As Decimal
+
+        'menampilkan header laporan penjualan
+        listDataPenjualan.Items.Add("LAPORAN PENJUALAN")
+        listDataPenjualan.Items.Add("-------------------------")
+
+        'menampilkan data penjualan untuk tiap produk
+        For intHitung = 0 To intSUBSKRIPT_MAKS
+
+            'menghitung pendapatan produk
+            decPendapatan = intUnitTerjual(intHitung) * decHarga(intHitung)
+
+            'menampilkan data produk
+            listDataPenjualan.Items.Add("Nomor Produk: " &
+            intNomorProd(intHitung))
+            listDataPenjualan.Items.Add("Nama: " &
+            strNamaProduk(intHitung))
+            listDataPenjualan.Items.Add("Deskripsi: " &
+            strPenjelasan(intHitung))
+            listDataPenjualan.Items.Add("Harga Per Unit: " &
+            "Rp. " & decHarga(intHitung).ToString())
+            listDataPenjualan.Items.Add("Banyak Unit Terjual: " &
+            "Rp. " & intUnitTerjual(intHitung).ToString())
+            listDataPenjualan.Items.Add("Pendapatan Produk: " &
+            "Rp. " & decPendapatan.ToString())
+            listDataPenjualan.Items.Add("")
+
+            'mengakumulasi pendapatan
+            decPendapatanTotal = decPendapatanTotal + decPendapatan
+        Next
+
+        'menampilkan pendapatan total
+        labelPendapatanTotal.Text = "Rp. " & decPendapatanTotal.ToString()
+    End Sub
+
+    Private Sub mnuHelpTentang_Click(sender As Object, e As EventArgs) Handles mnuHelpTentang.Click
+        'menampilkan kotak tentang
+        MessageBox.Show("Menampilkan laporan penjualan untuk PKT.", "Tentang")
+    End Sub
+End Class
